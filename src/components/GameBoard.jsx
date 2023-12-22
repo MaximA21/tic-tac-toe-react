b2b8ac79052da61368fc369386498e182a2666ba
Maxim <118841284+MaximA21@users.noreply.github.com>
2023-12-22 21:12:03 +0100

    add: ability to interact with the gameboard

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,5 +1,40 @@
-export function GameBoard() {
+import {useState} from "react";
+
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+]
+
+
+export default function GameBoard() {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard)
+
+    function handleSelectSquare(rowIndex, colIndex) {
+        setGameBoard(prevState => {
+            const updateBoard = [...prevState.map(innerArray => [...innerArray])]
+            updateBoard[rowIndex][colIndex] = "X"
+            return updateBoard
+        })
+    }
+
+
+
     return (
-        <></>
+        <ol id="game-board">
+            {gameBoard.map((row, rowIndex) => (
+
+                <li key={rowIndex}>
+
+                    <ol>
+                        {row.map((playerSymbol, colIndex) => (
+                            <li key={colIndex}>
+                                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                            </li>
+                        ))}
+                    </ol>
+                </li>
+            ))}
+        </ol>
     )
 }
